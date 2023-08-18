137bcce04d00bf231b37a0f487988142836722e5
JAY <jayjatharjj>
2023-08-18 23:01:59 +0530

    Modified footer

--- React/src/Components/Footer.js
+++ React/src/Components/Footer.js
@@ -4,7 +4,7 @@ export default class Footer extends Component {
   render() {
     return (
       <div className='footer'>
-        <span>Copyright ©2023 All rights reserved | This page is made with <img src='https://cdn-icons-png.flaticon.com/512/2589/2589175.png' alt='love' width='15px' height='15px'></img> by Group 19</span>
+        <span>Copyright ©2023 All rights reserved | This page is made with <img src='https://cdn-icons-png.flaticon.com/512/2589/2589175.png' alt='love' width='15px' height='15px'></img> by Jay Jathar</span>
       </div>
     )
   }
